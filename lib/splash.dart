56772ebf0af1fa1270ebc3c3cac929929e08cdfb
wooxist <tossy@tossy.me>
2021-03-03 12:46:01 +0900

    wip

--- lib/splash.dart
+++ lib/splash.dart
@@ -8,6 +8,5 @@ export 'package:stacked_mvvm/stacked_mvvm.dart';
 export 'error/app_error.dart';
 export 'error/app_error.dart';
 export 'view_model/view_model.dart';
-export 'package:flutter_branch_sdk/flutter_branch_sdk.dart';
 
 typedef Route<dynamic> GenerateRoutes(RouteSettings settings);
